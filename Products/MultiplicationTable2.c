7f7b6efa97b281103a09dfa4b567f73f022e5096
itsme-Subid <itsmesubid@gmail.com>
2021-10-13 00:35:09 +0530

    Initial Commit

---
+++ Products/MultiplicationTable2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+
+    float n;
+    int max;
+    while (1)
+    {
+        printf("Enter the number whose multiplication table you want (press 0 to exit)\n");
+        scanf("%f", &n);
+        if (n == 0)
+        {
+            break;
+        }
+        printf("Enter how much multiplication you want\n");
+        scanf("%d", &max);
+        for (int m = 1; m <= max; m++)
+        {
+            printf("%.2f X %d = %.3f \n", n, m, n * m);
+        }
+    }
+}
