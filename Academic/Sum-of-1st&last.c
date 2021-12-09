bd46fd5656132a63045fbfa020275087297700b8
itsme-Subid <itsmesubid@gmail.com>
2021-12-09 23:00:26 +0530

    Initial Commit

--- Academic/Sum-of-1st&last.c
+++ Academic/Sum-of-1st&last.c
@@ -3,13 +3,10 @@
 void main()
 {
     //sum of 1st and last digit of a five digit number
-    int a, b, c, d;
+    int a, b;
     printf("Enter any 5 digit integer\n");
     scanf("%d", &a);
-    b = a / 10000;
-    c = a % 10;
-    d = b + c;
-    printf("1st = %d\nlast = %d\n", b, c);
-    printf("sum of 1st and last digit of %d five digit number is %d\n", a, d);
+    b = (a / 10000) + (a % 10);
+    printf("sum of 1st and last digit of %d five digit number is %d\n", a, b);
     getch();
 }
