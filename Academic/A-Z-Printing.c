6eead5aba829caf71c1f29c25407e62b9ea12937
SUBID DAS <itsmesubid@gmail.com>
2021-12-15 23:43:51 +0530

    Create A-Z-Printing.c

---
+++ Academic/A-Z-Printing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int 😎 = 0;
+    printf("Enter a Character\n");
+    scanf("%c",&😎);
+    printf("\n\n");
+    for (int i = 0; i < 26; i++)
+    {
+        printf("\t%c\n", 😎);
+        😎++;
+    }
+
+    getch();
+}
