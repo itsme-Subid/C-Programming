6adf2bf59696c3822cabcf2afac3264f176bae08
SUBID DAS <itsmesubid@gmail.com>
2022-03-09 21:20:54 +0530

    Initial Commit

--- Products/add-2-vectors.c
+++ Products/add-2-vectors.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <conio.h>
 
-struct vec
-{
-    int x, y, z;
-    char i, j, k;
-};
-
 int main()
 {
-    struct vec arr[2];
+    int x1, x2, y1, y2, z1, z2, i1, i2, j1, j2, k1, k2;
+
     printf("\n\nNote: if you have one dimensional or two dimensional vector\nThen write the other component as 0\nFor example 4j -> 0i+4j+0k\n");
     printf("Enter the vector A\n");
-    scanf("%d%c%d%c%d%c", &arr[0].x, &arr[0].i, &arr[0].y, &arr[0].j, &arr[0].z, &arr[0].k);
+    scanf("%d%c%d%c%d%c", &x1, &i1, &y1, &j1, &z1, &k1);
     printf("Enter the vector B\n");
-    scanf("%d%c%d%c%d%c", &arr[1].x, &arr[1].i, &arr[1].y, &arr[1].j, &arr[1].z, &arr[1].k);
+    scanf("%d%c%d%c%d%c", &x2, &i2, &y2, &j2, &z2, &k2);
 
-    arr[0].x = arr[0].x + arr[1].x;
-    arr[0].y = arr[0].y + arr[1].y;
-    arr[0].z = arr[0].z + arr[1].z;
+    x1 = x1 + x2;
+    y1 = y1 + y2;
+    z1 = z1 + z2;
 
-    printf("(%d%c)+(%d%c)+(%d%c)\n", arr[0].x, arr[0].i, arr[0].y, arr[0].j, arr[0].z, arr[0].k);
+    printf("(%d%c)+(%d%c)+(%d%c)\n", x1, i1, y1, j1, z1, k1);
     return 0;
 }
